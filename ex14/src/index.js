f43341fe0f6837644b0ac01c9dc2b750854b480b
Kemal Arifovic <gigalab@gmail.com>
2021-03-12 15:55:28 +0100

    Word Blanks

---
+++ ex14/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+  // Only change code below this code
+  var myNoun = "dog";
+  var myVerb = "ran";
+  var myAdjective = "big";
+  var myAdverb = "quickly";
+  var wordBlanks = "Today " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + ".";
+  // Only change code above this code
+  return wordBlanks;
+}
+console.log(main());
+module.exports = main;
